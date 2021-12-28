832615bc674d5eb48607ea03ffb268577a7fbdf7
joelgroomer <joelgroomer@users.noreply.github.com>
2021-12-28 15:28:16 -0800

    Message model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  has_many :messages
   validates :username, 
             presence: true, 
             uniqueness: { case_sensitive: false }, 
             length: { minimum: 3, maximum: 15 }
   
   has_secure_password
-  
+
 end
